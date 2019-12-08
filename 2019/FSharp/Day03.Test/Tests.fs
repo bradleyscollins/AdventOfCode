759feaafaeca4dd3aade6ca2e8cca263a1b74661
Collins <collinsb@in.dynetics.com>
2019-12-08 00:29:40 -0600

    2019, F#, Day 3: Part 1 complete

--- 2019/FSharp/Day03.Test/Tests.fs
+++ 2019/FSharp/Day03.Test/Tests.fs
@@ -4,6 +4,131 @@ open System
 open Xunit
 open Swensen.Unquote
 
+module Tuple2 =
+    let toObjArray (x, y) = [| box x; box y |]
+
+module Tuple3 =
+    let toObjArray (x, y, z) = [| box x; box y; box z |]
+
+let ``Segment parse test cases`` =
+    [|
+        "R75", Some (Right 75)
+        "D30", Some (Down  30)
+        "R83", Some (Right 83)
+        "U83", Some (Up    83)
+        "L12", Some (Left  12)
+        "D49", Some (Down  49)
+        "R71", Some (Right 71)
+        "U7" , Some (Up     7)
+        "L72", Some (Left  72)
+        "X27", None
+        "Rxx", None
+    |]
+    |> Seq.map Tuple2.toObjArray
+
+[<Theory>]
+[<MemberData("Segment parse test cases")>]
+let ``Segment parse converts a string to Segment option`` str expected =
+    Segment.parse str =! expected
+
+
+let ``Wire parse test cases`` =
+    [|
+        "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+        Some [ Right 75
+               Down  30
+               Right 83
+               Up    83
+               Left  12
+               Down  49
+               Right 71
+               Up     7
+               Left  72 ]
+
+        "U62,R66,U55,R34,D71,R55,D58,R83",
+        Some [ Up    62
+               Right 66
+               Up    55
+               Right 34
+               Down  71
+               Right 55
+               Down  58
+               Right 83 ]
+
+        "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+        Some [ Right 98
+               Up    47
+               Right 26
+               Down  63
+               Right 33
+               Up    87
+               Left  62
+               Down  20
+               Right 33
+               Up    53
+               Right 51 ]
+
+        "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+        Some [ Up    98
+               Right 91
+               Down  20
+               Right 16
+               Down  67
+               Right 40
+               Up     7
+               Right 15
+               Up     6
+               Right  7 ]
+
+        "U98 R91", None
+        "X98,R91", None
+        "U98,Rxx", None
+    |]
+    |> Seq.map Tuple2.toObjArray
+
+[<Theory>]
+[<MemberData("Wire parse test cases")>]
+let ``Wire parse converts a comma-separated string to list of Segments`` str expected =
+    Wire.parse str =! expected
+
 [<Fact>]
-let ``My test`` () =
-    true =! false
+let ``Wire allPoints builds a list of all points in the wire starting at the given point (except the starting point)`` () =
+    let wire = Right 8 :: Up 5 :: Left 5 :: Down 3 :: []
+    let expected = [ (1,0); (2,0); (3,0); (4,0); (5,0); (6,0); (7,0); (8,0);
+                     (8,1); (8,2); (8,3); (8,4); (8,5);
+                     (7,5); (6,5); (5,5); (4,5); (3,5);
+                     (3,4); (3,3); (3,2) ]
+    wire |> Wire.allPointsStartingFrom (0,0) =! expected
+
+[<Theory>]
+[<InlineData(0, 0,  3, 3,   6)>]
+[<InlineData(3, 3,  0, 0,   6)>]
+[<InlineData(1, 1,  4, 4,   6)>]
+[<InlineData(4, 4,  1, 1,   6)>]
+[<InlineData(1, 1,  7, 6,  11)>]
+[<InlineData(7, 6,  1, 1,  11)>]
+let ``Point manhattanDistanceTo calculates the Manhattan distance between to points`` x1 y1 x2 y2 expected =
+    (x1, y1) |> Point.manhattanDistanceTo (x2, y2) =! expected
+
+
+
+let ``Points along test cases`` =
+    [|
+        (Up    3), [ ( 0,  1); ( 0,  2); ( 0,  3) ]
+        (Down  3), [ ( 0, -1); ( 0, -2); ( 0, -3) ]
+        (Left  3), [ (-1,  0); (-2,  0); (-3,  0) ]
+        (Right 3), [ ( 1,  0); ( 2,  0); ( 3,  0) ]
+    |]
+    |> Seq.map Tuple2.toObjArray
+
+[<Theory>]
+[<MemberData("Points along test cases")>]
+let ``Points along returns the list of points along a segment starting with a given point`` segment expected =
+    (0, 0) |> Points.along segment =! expected
+
+
+[<Theory>]
+[<InlineData("R75,D30,R83,U83,L12,D49,R71,U7,L72",          "U62,R66,U55,R34,D71,R55,D58,R83",      159)>]
+[<InlineData("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7", 135)>]
+let ``calcDistanceToNearestIntersection determines the Manhattan Distance to the nearst intersection of two wires`` wire1 wire2 expected =
+    calcDistanceToNearestIntersection wire1 wire2 =! expected
--- 2019/FSharp/Day03/Program.fs
+++ 2019/FSharp/Day03/Program.fs
@@ -1,8 +1,128 @@
 ﻿module Day03
 
 open System
+open System.IO
+open System.Text.RegularExpressions
+
+module String =
+    let split (delimiter : string) (s : string) = s.Split delimiter
+    let trim (s : string) = s.Trim ()
+
+module List =
+    let cons h t = h :: t
+
+module Seq =
+    let sequence options =
+        let rec transform values opts =
+            match opts with
+            | [] -> Some values
+            | None :: _ -> None
+            | Some x :: tail -> transform (x :: values) tail
+            
+        options
+        |> Seq.toList
+        |> transform []
+        |> Option.map List.rev
+
+type Segment =
+| Up of int
+| Down of int
+| Left of int
+| Right of int
+
+module Segment =
+    let parse str =
+        let re = Regex @"^(?<direction>[LRUD])(?<length>\d+)$"
+        let result = re.Match str
+
+        match result.Success with
+        | true -> 
+            let length = int <| result.Groups.["length"].Value
+            let direction = result.Groups.["direction"].Value
+
+            match direction with
+            | "R" -> Some (Right length)
+            | "L" -> Some (Left  length)
+            | "U" -> Some (Up    length)
+            | "D" -> Some (Down  length)
+            | _ -> None
+        | _ -> None
+
+
+type Point = int * int
+
+module Point =
+    let up    (x, y) distance = (x,            y + distance)
+    let down  (x, y) distance = (x,            y - distance)
+    let right (x, y) distance = (x + distance, y           )
+    let left  (x, y) distance = (x - distance, y           )
+
+    let manhattanDistanceTo (x2, y2) (x1, y1) =
+        abs (x2 - x1) + abs (y2 - y1)
+
+module Points =
+    let along segment startingAt =
+        let move, distance =
+            match segment with
+            | Up by    -> Point.up, by
+            | Down by  -> Point.down, by
+            | Left by  -> Point.left, by
+            | Right by -> Point.right, by
+
+        [1 .. distance]
+        |> List.map (move startingAt)
+        
+
+type Wire = Segment list
+
+module Wire =
+    let empty = []
+    let parse str =
+        String.split "," str
+        |> Seq.map Segment.parse
+        |> Seq.sequence
+
+    let allPointsStartingFrom startingAt wire =
+        wire
+        |> List.fold
+            (fun points segment ->
+                let startingPoint = points |> List.last
+                let segmentPoints = startingPoint |> Points.along segment
+                points @ segmentPoints)
+            [ startingAt ]
+        |> List.tail
+
+
+let calcDistanceToNearestIntersection wire1 wire2 =
+    let centralPort = 0,0
+    let pointsInWire1 = wire1
+                        |> Wire.parse
+                        |> Option.defaultValue Wire.empty
+                        |> Wire.allPointsStartingFrom centralPort
+                        |> Set.ofList
+    let pointsInWire2 = wire2
+                        |> Wire.parse
+                        |> Option.defaultValue Wire.empty
+                        |> Wire.allPointsStartingFrom centralPort
+                        |> Set.ofList
+    let intersections = Set.intersect pointsInWire1 pointsInWire2
+    
+    intersections
+    |> Seq.map (Point.manhattanDistanceTo centralPort)
+    |> Seq.min
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let wires = File.ReadAllLines "input.txt"
+                |> Seq.map String.trim
+                |> Seq.filter (not << String.IsNullOrEmpty)
+                |> Seq.toList
+
+    match wires with
+    | wire1 :: wire2 :: _ ->
+        calcDistanceToNearestIntersection wire1 wire2
+        |> printfn "Manhattan distance to nearest intersection: %d"
+
+    | _ -> eprintfn "Bad input!"
+
     0 // return an integer exit code
