701db911366b1c398483befa1a7d1df9985391be
Collins <collinsb@in.dynetics.com>
2019-12-24 09:04:13 -0600

    2019, F#, Day 4: Part 1 complete

--- 2019/FSharp/Day04.Test/Tests.fs
+++ 2019/FSharp/Day04.Test/Tests.fs
@@ -1,8 +1,31 @@
 module Day04.Tests
 
 open System
+open Swensen.Unquote
 open Xunit
 
-[<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+[<Theory>]
+[<InlineData("122345",  true)>]
+[<InlineData("111123",  true)>]
+[<InlineData("135679",  false)>]
+[<InlineData("111111",  true)>]
+[<InlineData("223450",  false)>]
+[<InlineData("123789",  false)>]
+[<InlineData("12378",   false)>]
+[<InlineData("1236789", false)>]
+let ``Password.isValid evaluates whether a password meets the criteria`` password expected =
+    Password.isValid password =! expected
+
+[<Theory>]
+[<InlineData("122345",  "122346")>]
+[<InlineData("277777",  "277778")>]
+[<InlineData("277778",  "277779")>]
+[<InlineData("277779",  "277788")>]
+[<InlineData("277788",  "277789")>]
+[<InlineData("277789",  "277799")>]
+[<InlineData("277799",  "277888")>]
+[<InlineData("277999",  "278888")>]
+[<InlineData("279999",  "288888")>]
+[<InlineData("299999",  "333333")>]
+let ``Password.next takes one password and generates the next valid one`` password expected =
+    Password.next password =! expected
--- 2019/FSharp/Day04/Program.fs
+++ 2019/FSharp/Day04/Program.fs
@@ -2,7 +2,74 @@
 
 open System
 
+module String =
+    let characters (str : string) = str.ToCharArray ()
+
+module Password =
+    let isCorrectLength password =
+        password 
+        |> String.length
+        |> ((=) 6)
+
+    let containsADouble password =
+        password
+        |> Seq.windowed 2
+        |> Seq.exists (Array.distinct >> Array.length >> ((=) 1))
+
+    let neverDecreases password =
+        password
+        |> String.characters
+        |> Seq.map (string >> int)
+        |> Seq.windowed 2
+        |> Seq.forall (fun pair -> pair.[0] <= pair.[1])
+
+    let isValid password =
+        isCorrectLength password
+        && containsADouble password
+        && neverDecreases password
+
+    let next password =
+        let next' = Int32.Parse password
+                    |> ((+) 1)
+                    |> string
+                    |> String.characters
+                    |> Seq.ofArray
+                    |> Seq.map (string >> int)
+                    |> Seq.toList
+
+        let rec makeNeverDescending acc rest =
+            match acc, rest with
+            | [], n :: tail -> makeNeverDescending [n] tail
+            | m :: _, n :: tail -> 
+                if m <= n
+                then makeNeverDescending (n :: acc) tail
+                else
+                    let repeated = List.init (List.length rest) (fun _ -> m)
+                    makeNeverDescending (repeated @ acc) []
+            | _, [] ->
+                Seq.rev acc
+                |> Seq.map string
+                |> String.concat ""
+
+        makeNeverDescending [] next'
+
+let validPasswordsInRange low high =
+    let high' = Int32.Parse high
+    let rec f acc password =
+        if Int32.Parse password > high'
+        then acc
+        else
+            let acc' = if password |> Password.isValid
+                       then (password :: acc)
+                       else acc
+            f acc' (password |> Password.next)
+
+    f [] low
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    validPasswordsInRange "271973" "785961"
+    |> List.length
+    |> printfn "Number of valid passwords in range 271973-785961: %A"
+
     0 // return an integer exit code
