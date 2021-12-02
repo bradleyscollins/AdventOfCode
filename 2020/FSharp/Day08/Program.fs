af6eeb831acbae484316b54bf77e29f047d92392
Brad Collins <brad.collins@dynetics.com>
2021-12-02 14:01:09 -0600

    2020, F#, Day 8: Partial solution

---
+++ 2020/FSharp/Day08/Program.fs
@@ -0,0 +1,14 @@
+﻿module Day08
+
+open System
+open System.IO
+open AdventOfCode.Utilities
+
+[<EntryPoint>]
+let main argv =
+    let lines = 
+        File.ReadAllLines "input.txt"
+        |> Seq.map String.trim
+        |> Seq.toArray
+
+    0 // return an integer exit code
