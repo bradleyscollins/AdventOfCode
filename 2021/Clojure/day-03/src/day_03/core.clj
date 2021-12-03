c4881dfb336646538525b0302ecf08399a25820e
Brad Collins <bsc@housecollins.com>
2021-12-03 08:50:50 -0600

    2021, Clojure, Day 3: Part 1 solved

--- 2021/Clojure/day-03/src/day_03/core.clj
+++ 2021/Clojure/day-03/src/day_03/core.clj
@@ -52,22 +52,76 @@
 ;; Use the binary numbers in your diagnostic report to calculate the gamma rate
 ;; and epsilon rate, then multiply them together. What is the power consumption
 ;; of the submarine? (Be sure to represent your answer in decimal, not binary.)
-;; ANSWER: 
+;; ANSWER: 3901196
 
 (ns day-03.core
   (:gen-class)
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.string  :as str]))
 
 (def input-file
   (io/resource "input.txt"))
 
 (defn read-input []
-  (->>
-   (with-open [rdr (io/reader input-file)]
-     (doall (line-seq rdr)))
-   (map #(Integer/parseInt %1))))
+  (with-open [rdr (io/reader input-file)]
+    (doall (line-seq rdr))))
+
+(defn digitize
+  "Converts a line of input into a list of individual digits"
+  [input]
+  (->> (str/split input #"")
+       (map #(Integer/parseInt %1))))
+
+(defn columnize
+  "Converts a list of inputs into columns of digits"
+  [inputs]
+  (->> (map digitize inputs)
+       (apply map list)))
+
+(defn histograms
+  "Converts columns into histograms"
+  [columns]
+  (map frequencies columns))
+
+(defn key-of-max-value
+  "Gets the key of the map entry with the greatest value. If there a multiple, the last one is returned."
+  [m]
+  (key (apply max-key val m)))
+
+(defn key-of-min-value
+  "Gets the key of the map entry with the least value. If there a multiple, the last one is returned."
+  [m]
+  (key (apply min-key val m)))
+(defn parse-binary [s] (Integer/parseInt s 2))
+
+(defn calc-gamma-rate
+  "Converts column histograms into a gamma rate"
+  [histograms]
+  (->> (map (comp str key-of-max-value) histograms)
+       (str/join)
+       (parse-binary)))
+
+(defn calc-epsilon-rate
+  "Converts column histograms into an epsilon rate"
+  [histograms]
+  (->> (map (comp str key-of-min-value) histograms)
+       (str/join)
+       (parse-binary)))
+
+(defn calc-power-consumption
+  "Calculates power consumption from a gamma rate & an epsilon rate"
+  [gamma-rate epsilon-rate]
+  (* gamma-rate epsilon-rate))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Read inputs and calculate the submarine's power consumption"
   [& args]
-  (println "Hello, World!"))
+  (let [inputs (read-input)
+        columns (columnize inputs)
+        histograms (histograms columns)
+        gamma-rate (calc-gamma-rate histograms)
+        epsilon-rate (calc-epsilon-rate histograms)
+        power-consumption (calc-power-consumption gamma-rate epsilon-rate)]
+    (println "Gamma rate (γ):            " gamma-rate)
+    (println "Epsilon rate (ε):          " epsilon-rate)
+    (println "Power consumption (γ × ε): " power-consumption)))
