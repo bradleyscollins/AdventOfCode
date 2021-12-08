8dea5e833f37cad51bd58b570274ea86ef43ef32
Brad Collins <bsc@housecollins.com>
2021-12-07 20:27:34 -0600

    2021, Clojure, Day 8: Setup

---
+++ 2021/Clojure/day-08/README.md
@@ -0,0 +1,3 @@
+# # [Day 8: ???](https://adventofcode.com/2021/day/8)
+
+[Puzzle Input](https://adventofcode.com/2021/day/8/input)
---
+++ 2021/Clojure/day-08/src/day_08/core.clj
@@ -0,0 +1,16 @@
+(ns day-08.core
+  (:gen-class)
+  (:require [clojure.java.io :as io]
+            [clojure.string  :as str]))
+
+(def input-file
+  (io/resource "input.txt"))
+
+(defn read-input []
+  (with-open [rdr (io/reader input-file)]
+    (doall (line-seq rdr))))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
