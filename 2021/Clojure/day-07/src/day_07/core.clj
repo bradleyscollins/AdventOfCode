7d5d39e745a41d1314f9ce7b08c04a4fdeb19bd0
Brad Collins <bsc@housecollins.com>
2021-12-07 20:22:45 -0600

    2021, Clojure, Day 7: Part 2 solved

--- 2021/Clojure/day-07/src/day_07/core.clj
+++ 2021/Clojure/day-07/src/day_07/core.clj
@@ -12,28 +12,37 @@
 
 (defn str->positions [line]
   (->> (str/split line #",")
-       (map #(Integer/parseInt %1))))
+       (map #(Long/parseLong %1))))
 
 (defn load-positions [lines]
   (mapcat str->positions lines))
 
-(defn median [xs]
-  (let [cnt (count xs)
-        midpoint (quot cnt 2)
-        sorted (vec (sort xs))]
-    (if (odd? cnt)
-      (nth sorted midpoint)
-      (let [submedian   (nth sorted (dec midpoint))
-            supermedian (nth sorted midpoint)]
-        (quot (+ submedian supermedian) 2)))))
+(defn calc-fuel-cost
+  ([destination current-pos]
+   (let [distance (Math/abs (- current-pos destination))]
+     (if (zero? distance)
+       0
+       (calc-fuel-cost distance))))
 
-(defn minimize-fuel [positions]
-  (let [med (median positions)]
-    (->> (map #(Math/abs (- %1 med)) positions)
-         (apply +))))
+  ([distance]
+   {:pre [(> distance 0)]}
+   (quot (* distance (inc distance))
+         2)))
+
+(defn calc-total-fuel-cost [positions destination]
+  (->> positions
+       (map (partial calc-fuel-cost destination))
+       (reduce +)))
+
+=(defn minimize-fuel [positions]
+  (let [min-pos (apply min positions)
+        max-pos (apply max positions)
+        domain  (range min-pos (inc max-pos))]
+    (->> (map (partial calc-total-fuel-cost positions) domain)
+         (apply min))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Minimize fuel cost to align crabs horizontally"
   [& args]
   (let [positions (load-positions (read-input))]
     (println "Minimum fuel: " (minimize-fuel positions))))
--- 2021/Clojure/day-07/test/day_07/core_test.clj
+++ 2021/Clojure/day-07/test/day_07/core_test.clj
@@ -2,10 +2,28 @@
   (:require [clojure.test :refer :all]
             [day-07.core :refer :all]))
 
-(deftest median-test
-  (testing "Calculates median"
-    (is (= 2 (median [16 1 2 0 4 2 7 1 2 14])))))
+(deftest calc-fuel-cost-test
+  (testing "Calculates the fuel cost to move n steps"
+    (is (= 1  (calc-fuel-cost 1)))
+    (is (= 3  (calc-fuel-cost 2)))
+    (is (= 6  (calc-fuel-cost 3)))
+    (is (= 10 (calc-fuel-cost 4)))
+    (is (= 15 (calc-fuel-cost 5)))
+    (is (= 21 (calc-fuel-cost 6)))
+    (is (= 45 (calc-fuel-cost 9)))
+    (is (= 66 (calc-fuel-cost 11)))))
+
+(def test-positions [16 1 2 0 4 2 7 1 2 14])
+
+(deftest calc-total-fuel-cost-test
+  (testing "Calculates the total fuel required to move all crabs to the given position"
+    (is (= 168 (calc-total-fuel-cost test-positions 5)))
+    (is (= 206 (calc-total-fuel-cost test-positions 2)))))
 
 (deftest minimize-fuel-test
   (testing "Calculates the minimum fuel required to align crabs horizontally"
-    (is (= 37 (minimize-fuel [16 1 2 0 4 2 7 1 2 14])))))
+    (is (= 168 (minimize-fuel test-positions)))))
+
+(deftest minimize-fuel-test
+  (testing "Calculates the minimum fuel required to align crabs horizontally"
+    (is (= 168 (minimize-fuel test-positions)))))
