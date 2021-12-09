0a237b27b030170c4dbb683f365b35f7f2734c0c
Brad Collins <bsc@housecollins.com>
2021-12-09 07:56:43 -0600

    2021, Clojure, Day 8: Part 2 solved

--- 2021/Clojure/day-08/src/day_08/core.clj
+++ 2021/Clojure/day-08/src/day_08/core.clj
@@ -1,6 +1,7 @@
 (ns day-08.core
   (:gen-class)
   (:require [clojure.java.io :as io]
+            [clojure.set     :as set]
             [clojure.string  :as str]))
 
 (def input-file
@@ -11,48 +12,75 @@
     (doall (line-seq rdr))))
 
 (defn str->signal-patterns [s]
-  (str/split s #" "))
-
-(defn signal-patterns->pattern-map [signal-patterns]
-  (let [grouped-by-length (group-by count signal-patterns)]
-    (assoc {}
-           (first (get grouped-by-length 2)) 1
-           (first (get grouped-by-length 3)) 7
-           (first (get grouped-by-length 4)) 4
-           (first (get grouped-by-length 7)) 8)))
-
-(def str->signal-pattern-map
-  (comp signal-patterns->pattern-map str->signal-patterns))
+  (map set (str/split s #" ")))
 
 (defn str->outputs [s]
-  (str/split s #" "))
+  (map set (str/split s #" ")))
 
 (defn str->domain [s]
   (let [[patterns-str outputs-str] (str/split s #" \| ")
-        pattern-map                (str->signal-pattern-map patterns-str)
+        patterns                   (str->signal-patterns patterns-str)
         outputs                    (str->outputs outputs-str)]
-    (vector pattern-map outputs)))
+    (vector patterns outputs)))
 
-(defn load-data []
-  (map str->domain (read-input)))
+(defn decode-unique-patterns [patterns-grouped-by-length]
+  (assoc {}
+         (first (patterns-grouped-by-length 2)) 1
+         (first (patterns-grouped-by-length 3)) 7
+         (first (patterns-grouped-by-length 4)) 4
+         (first (patterns-grouped-by-length 7)) 8))
 
-(defn decode-output [output]
-  (case (count output)
-    2 1
-    3 7
-    4 4
-    7 8
-    nil))
+(defn decode-length-5-pattern [pattern-map pattern]
+  (let [num->pattern-map (set/map-invert pattern-map)]
+    (case [(count (set/intersection pattern (num->pattern-map 4)))
+           (count (set/intersection pattern (num->pattern-map 7)))]
+      [2 2] (assoc pattern-map pattern 2)
+      [3 3] (assoc pattern-map pattern 3)
+      [3 2] (assoc pattern-map pattern 5)
+      pattern-map)))
+
+(defn decode-length-5-patterns [length-5-patterns pattern-map]
+  (reduce decode-length-5-pattern pattern-map length-5-patterns))
+
+(defn decode-length-6-pattern [pattern-map pattern]
+  (let [num->pattern-map (set/map-invert pattern-map)
+        pattern-1        (num->pattern-map 1)
+        pattern-3        (num->pattern-map 3)]
+
+    (cond
+      (= 1 (count (set/intersection pattern pattern-1)))
+      (assoc pattern-map pattern 6)
+
+      (= 4 (count (set/intersection pattern pattern-3)))
+      (assoc pattern-map pattern 0)
+
+      (= 5 (count (set/intersection pattern pattern-3)))
+      (assoc pattern-map pattern 9)
+
+      :else pattern-map)))
+
+(defn decode-length-6-patterns [length-6-patterns pattern-map]
+  (reduce decode-length-6-pattern pattern-map length-6-patterns))
+
+(defn signal-patterns->pattern-map [signal-patterns]
+  (let [grouped-by-length (group-by count signal-patterns)]
+    (->> (decode-unique-patterns grouped-by-length)
+         (decode-length-5-patterns (grouped-by-length 5))
+         (decode-length-6-patterns (grouped-by-length 6)))))
 
 (defn decode-display [pattern-map outputs]
-  (->> (map decode-output outputs)
-       (filter some?)))
+  (map pattern-map outputs))
+
+(defn decode [input]
+  (let [[signal-patterns outputs] (str->domain input)
+        pattern-map  (signal-patterns->pattern-map signal-patterns)
+        display                   (decode-display pattern-map outputs)]
+    (Integer/parseInt (str/join display))))
 
 (defn -main
   "Decodes scrambled 7-segment display"
   [& args]
-  (let [pairs               (map str->domain (read-input))
-        decoded-displays    (map #(apply decode-display %1) pairs)
-        decoded-digit-count (reduce + (map count decoded-displays))]
-    (println "Decoded displays:" decoded-displays)
-    (println "Total number of decoded digits:" decoded-digit-count)))
+  (let [lines (read-input)
+        values (map decode lines)]
+    (println "Values:" values)
+    (println "Sum of values:" (reduce + values))))
--- 2021/Clojure/day-08/test/day_08/core_test.clj
+++ 2021/Clojure/day-08/test/day_08/core_test.clj
@@ -2,105 +2,56 @@
   (:require [clojure.test :refer :all]
             [day-08.core :refer :all]))
 
-(def test-input
-  (list
-   "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe"
-   "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc"
-   "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg"
-   "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb"
-   "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea"
-   "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb"
-   "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe"
-   "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef"
-   "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb"
-   "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"))
+(deftest str->domain-test
+  (testing "Converts a line of input to a list of patterns and a list of outputs"
+    (is (= [[(set "acedgfb")
+             (set "cdfbe")
+             (set "gcdfa")
+             (set "fbcad")
+             (set "dab")
+             (set "cefabd")
+             (set "cdfgeb")
+             (set "eafb")
+             (set "cagedb")
+             (set "ab")]
+            [(set "cdfeb") (set "fcadb") (set "cdfeb") (set "cdbaf")]]
+           (str->domain "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf")))))
 
 (deftest signal-patterns->pattern-map-test
   (testing "Converts a list of signal patterns into a pattern map"
-    (is (= {"be" 1, "edb" 7, "cgeb" 4, "cfbegad" 8}
-           (signal-patterns->pattern-map ["be" "cfbegad" "cbdgef" "fgaecd" "cgeb" "fdcge" "agebfd" "fecdb" "fabcd" "edb"])))
-    (is (= {"gc" 1, "cbg" 7, "gfec" 4, "gcadebf" 8}
-           (signal-patterns->pattern-map ["edbfga" "begcd" "cbg" "gc" "gcadebf" "fbgde" "acbgfd" "abcde" "gfcbed" "gfec"])))
-    (is (= {"cg" 1, "gcb" 7 "gfac" 4, "cdgabef" 8}
-           (signal-patterns->pattern-map ["fgaebd" "cg" "bdaec" "gdafb" "agbcfd" "gdcbef" "bgcad" "gfac" "gcb" "cdgabef"])))
-    (is (= {"bc" 1, "cbd" 7, "afcb" 4, "fcdbega" 8}
-           (signal-patterns->pattern-map ["fbegcd" "cbd" "adcefb" "dageb" "afcb" "bc" "aefdc" "ecdab" "fgdeca" "fcdbega"])))
-    (is (= {"gf" 1, "fbg" 7, "fcge" 4, "aecbfdg" 8}
-           (signal-patterns->pattern-map ["aecbfdg" "fbg" "gf" "bafeg" "dbefa" "fcge" "gcbea" "fcaegb" "dgceab" "fcbdga"])))
-    (is (= {"ca" 1, "acf" 7, "baec" 4, "bdacfeg" 8}
-           (signal-patterns->pattern-map ["fgeab" "ca" "afcebg" "bdacfeg" "cfaedg" "gcfdb" "baec" "bfadeg" "bafgc" "acf"])))
-    (is (= {"gf" 1, "fgd" 7, "fgec" 4, "bdegcaf" 8}
-           (signal-patterns->pattern-map ["dbcfg" "fgd" "bdegcaf" "fgec" "aegbdf" "ecdfab" "fbedc" "dacgb" "gdcebf" "gf"])))
-    (is (= {"ed" 1, "ced" 7, "bedf" 4, "adcbefg" 8}
-           (signal-patterns->pattern-map ["bdfegc" "cbegaf" "gecbf" "dfcage" "bdacg" "ed" "bedf" "ced" "adcbefg" "gebcd"])))
-    (is (= {"cg" 1, "cgb" 7, "cegd" 4, "gbdefca" 8}
-           (signal-patterns->pattern-map ["egadfb" "cdbfeg" "cegd" "fecab" "cgb" "gbdefca" "cg" "fgcdab" "egfdb" "bfceg"])))
-    (is (= {"gf" 1, "gcf" 7, "gaef" 4, "dcaebfg" 8}
-           (signal-patterns->pattern-map ["gcafb" "gcf" "dcaebfg" "ecagb" "gf" "abcdeg" "gaef" "cafbge" "fdbac" "fegbdc"])))))
-
-(deftest str->signal-pattern-map-test
-  (testing "Converts a string into a pattern map"
-    ;; (is (= {"be" 1, "edb" 7, "cgeb" 4, "cfbegad" 8}
-    ;;        (signal-patterns->pattern-map ["be" "cfbegad" "cbdgef" "fgaecd" "cgeb" "fdcge" "agebfd" "fecdb" "fabcd" "edb"])))
-    ;; (is (= {"gc" 1, "cbg" 7, "gfec" 4, "gcadebf" 8}
-    ;;        (signal-patterns->pattern-map ["edbfga" "begcd" "cbg" "gc" "gcadebf" "fbgde" "acbgfd" "abcde" "gfcbed" "gfec"])))
-    ;; (is (= {"cg" 1, "gcb" 7 "gfac" 4, "cdgabef" 8}
-    ;;        (signal-patterns->pattern-map ["fgaebd" "cg" "bdaec" "gdafb" "agbcfd" "gdcbef" "bgcad" "gfac" "gcb" "cdgabef"])))
-    ;; (is (= {"bc" 1, "cbd" 7, "afcb" 4, "fcdbega" 8}
-    ;;        (signal-patterns->pattern-map ["fbegcd" "cbd" "adcefb" "dageb" "afcb" "bc" "aefdc" "ecdab" "fgdeca" "fcdbega"])))
-    ;; (is (= {"gf" 1, "fbg" 7, "fcge" 4, "aecbfdg" 8}
-    ;;        (signal-patterns->pattern-map ["aecbfdg" "fbg" "gf" "bafeg" "dbefa" "fcge" "gcbea" "fcaegb" "dgceab" "fcbdga"])))
-    ;; (is (= {"ca" 1, "acf" 7, "baec" 4, "bdacfeg" 8}
-    ;;        (signal-patterns->pattern-map ["fgeab" "ca" "afcebg" "bdacfeg" "cfaedg" "gcfdb" "baec" "bfadeg" "bafgc" "acf"])))
-    ;; (is (= {"gf" 1, "fgd" 7, "fgec" 4, "bdegcaf" 8}
-    ;;        (signal-patterns->pattern-map ["dbcfg" "fgd" "bdegcaf" "fgec" "aegbdf" "ecdfab" "fbedc" "dacgb" "gdcebf" "gf"])))
-    ;; (is (= {"ed" 1, "ced" 7, "bedf" 4, "adcbefg" 8}
-    ;;        (signal-patterns->pattern-map ["bdfegc" "cbegaf" "gecbf" "dfcage" "bdacg" "ed" "bedf" "ced" "adcbefg" "gebcd"])))
-    ;; (is (= {"cg" 1, "cgb" 7, "cegd" 4, "gbdefca" 8}
-    ;;        (signal-patterns->pattern-map ["egadfb" "cdbfeg" "cegd" "fecab" "cgb" "gbdefca" "cg" "fgcdab" "egfdb" "bfceg"])))
-    (is (= {"gf" 1, "gcf" 7, "gaef" 4, "dcaebfg" 8}
-           (signal-patterns->pattern-map "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc")))))
+    (is (= {(set "acedgfb") 8
+            (set "cdfbe")   5
+            (set "gcdfa")   2
+            (set "fbcad")   3
+            (set "dab")     7
+            (set "cefabd")  9
+            (set "cdfgeb")  6
+            (set "eafb")    4
+            (set "cagedb")  0
+            (set "ab")      1}
+           (signal-patterns->pattern-map [(set "acedgfb")
+                                          (set "cdfbe")
+                                          (set "gcdfa")
+                                          (set "fbcad")
+                                          (set "dab")
+                                          (set "cefabd")
+                                          (set "cdfgeb")
+                                          (set "eafb")
+                                          (set "cagedb")
+                                          (set "ab")])))))
 
 (deftest decode-display-test
-  (testing "Converts a list of signal patterns into a pattern map"
-    (is (= [8 4]
-           (decode-display {"be" 1, "edb" 7, "cgeb" 4, "cfbegad" 8}
-                           ["fdgacbe" "cefdb" "cefbgd" "gcbe"])))
-    (is (= [7 8 1]
-           (decode-display {"gc" 1, "cbg" 7, "gfec" 4, "gcadebf" 8}
-                           ["fcgedb" "cgb" "dgebacf" "gc"])))
-    (is (= [1 1 7]
-           (decode-display {"cg" 1, "gcb" 7 "gfac" 4, "cdgabef" 8}
-                           ["cg" "cg" "fdcagb" "cbg"])))
-    (is (= [1]
-           (decode-display {"bc" 1, "cbd" 7, "afcb" 4, "fcdbega" 8}
-                           ["efabcd" "cedba" "gadfec" "cb"])))
-    (is (= [4 8 7]
-           (decode-display {"gf" 1, "fbg" 7, "fcge" 4, "aecbfdg" 8}
-                           ["gecf" "egdcabf" "bgf" "bfgea"])))
-    (is (= [8 4 1 8]
-           (decode-display {"ca" 1, "acf" 7, "baec" 4, "bdacfeg" 8}
-                           ["gebdcfa" "ecba" "ca" "fadegcb"])))
-    (is (= [4 4 8]
-           (decode-display {"gf" 1, "fgd" 7, "fgec" 4, "bdegcaf" 8}
-                           ["cefg" "dcbef" "fcge" "gbcadfe"])))
-    (is (= [1]
-           (decode-display {"ed" 1, "ced" 7, "bedf" 4, "adcbefg" 8}
-                           ["ed" "bcgafe" "cdgba" "cbgef"])))
-    (is (= [8 7 1 7]
-           (decode-display {"cg" 1, "cgb" 7, "cegd" 4, "gbdefca" 8}
-                           ["gbdfcae" "bgc" "cg" "cgb"])))
-    (is (= [4 1]
-           (decode-display {"gf" 1, "gcf" 7, "gaef" 4, "dcaebfg" 8}
-                           ["fgae" "cfgab" "fg" "bagce"])))))
+  (testing "Given a signal pattern map, decodes a list of outputs into a list of digits"
+    (let [input "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
+          [patterns outputs] (str->domain input)
+          pattern-map (signal-patterns->pattern-map patterns)]
+      (is (= [5 3 5 3] (decode-display pattern-map outputs))))))
 
-(deftest str->signal-pattern-map-test
-  (testing "Converts a list of signal patterns into a pattern map"
-    (is (= {"be" 1, "edb" 7, "cgeb" 4, "cfbegad" 8}
-           (str->signal-pattern-map "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb")))))
-
-(deftest str->domain-test
-  (testing "Converts a list of signal patterns into a pattern map"
-    (is (= [{"be" 1, "edb" 7, "cgeb" 4, "cfbegad" 8}
-            ["fdgacbe" "cefdb" "cefbgd" "gcbe"]]
-           (str->domain "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe")))))
+(deftest decode-test
+  (testing "Given an input, determines the number on the display"
+    (is (= 8394
+           (decode "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe")))
+    (is (= 9781
+           (decode "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc")))
+    (is (= 5353
+           (decode "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf")))))
