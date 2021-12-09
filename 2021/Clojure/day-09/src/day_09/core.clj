ab4e79db2cfd02f09932466da2b0400dbbc0668a
Brad Collins <bsc@housecollins.com>
2021-12-09 14:32:08 -0600

    2021, Clojure, Day 9: Part 1 solved

--- 2021/Clojure/day-09/src/day_09/core.clj
+++ 2021/Clojure/day-09/src/day_09/core.clj
@@ -1,10 +1,75 @@
 (ns day-09.core
   (:gen-class)
   (:require [clojure.java.io :as io]
-            [clojure.set     :as set]
             [clojure.string  :as str]))
 
+(def input-file
+  (io/resource "input.txt"))
+
+(defn read-input []
+  (with-open [rdr (io/reader input-file)]
+    (doall (line-seq rdr))))
+
+(defn str->digits [s]
+  (map #(Integer/parseInt %1) (str/split s #"")))
+
+(defn heightmap-init [rows]
+  (to-array-2d (map str->digits rows)))
+
+(defn heightmap-dimensions [heightmap]
+  (let [rows (alength heightmap)
+        cols (alength (aget heightmap 0))]
+    [rows cols]))
+
+(defn heightmap-height-at [heightmap [r c]]
+  (aget heightmap r c))
+
+(defn heightmap-points-adjacent-to [heightmap [r c]]
+  (let [row-ahead         (dec r)
+        row-behind        (inc r)
+        col-left          (dec c)
+        col-right         (inc c)
+        ahead             [row-ahead  c]
+        behind            [row-behind c]
+        left              [r          col-left]
+        right             [r          col-right]
+        [row-len col-len] (heightmap-dimensions heightmap)
+        adjacent-points   [ahead behind left right]]
+    (letfn [(in-bounds? [[r' c']]
+              (and (<= 0 r')
+                   (< r' row-len)
+                   (<= 0 c')
+                   (< c' col-len)))]
+      (filter in-bounds? adjacent-points))))
+
+(defn heightmap-heights-adjacent-to [heightmap [r c]]
+  (->> (heightmap-points-adjacent-to heightmap [r c])
+       (map (partial heightmap-height-at heightmap))))
+
+(defn heightmap-low-point? [heightmap point]
+  (let [current-height (heightmap-height-at heightmap point)]
+    (cond
+      (zero? current-height) true
+      (= 9 current-height)   false
+      :else (let [adjacent-heights (heightmap-heights-adjacent-to heightmap point)]
+              (every? (partial < current-height) adjacent-heights)))))
+
+(defn heightmap-low-points [heightmap]
+  (let [[rows cols] (heightmap-dimensions heightmap)
+        all-points (for [r (range rows), c (range cols)] [r c])]
+    (->> all-points
+         (filter (partial heightmap-low-point? heightmap))
+         (map (partial heightmap-height-at heightmap)))))
+
+(defn low-point-risk-level [height]
+  (inc height))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Find the low points in a cave"
   [& args]
-  (println "Hello, World!"))
+  (let [heightmap (heightmap-init (read-input))
+        low-points (heightmap-low-points heightmap)
+        risk-levels (map low-point-risk-level low-points)]
+    (println "Low points in the cave:" low-points)
+    (println "Low point risk levels: " risk-levels)
+    (println "Total risk level:      " (reduce + risk-levels))))
--- 2021/Clojure/day-09/test/day_09/core_test.clj
+++ 2021/Clojure/day-09/test/day_09/core_test.clj
@@ -2,6 +2,42 @@
   (:require [clojure.test :refer :all]
             [day-09.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest str->digits-test
+  (testing "Converts a string of digits '1234' into their individiual digits [1 2 3 4]"
+    (is (= [2 1 9 9 9 4 3 2 1 0] (str->digits "2199943210")))
+    (is (= [9 8 5 6 7 8 9 8 9 2] (str->digits "9856789892")))
+    (is (= [9 8 9 9 9 6 5 6 7 8] (str->digits "9899965678")))))
+
+(def test-heightmap (heightmap-init ["2199943210"
+                                     "3987894921"
+                                     "9856789892"
+                                     "8767896789"
+                                     "9899965678"]))
+
+(deftest heightmap-dimensions-test
+  (testing "Gets the dimensions [rows cols] a heightmap"
+    (is (= [5 10] (heightmap-dimensions test-heightmap)))))
+
+(deftest heightmap-height-at-test
+  (testing "Gets the height at a given point [row col]"
+    (is (= 2 (heightmap-height-at test-heightmap [0 0])))
+    (is (= 8 (heightmap-height-at test-heightmap [1 2])))
+    (is (= 7 (heightmap-height-at test-heightmap [2 4])))
+    (is (= 6 (heightmap-height-at test-heightmap [3 6])))
+    (is (= 7 (heightmap-height-at test-heightmap [4 8])))))
+
+(deftest heightmap-points-adjacent-to-test
+  (testing "Calculates the points adjacent to (ahead, behind, left, right) the point [r c]"
+    (is (= [[1 0] [0 1]] (heightmap-points-adjacent-to test-heightmap [0 0])))))
+
+(deftest heightmap-low-point?-test
+  (testing "Determines whether the height at a given point [row col] is a low point"
+    (is (heightmap-low-point? test-heightmap [0 1]))
+    (is (heightmap-low-point? test-heightmap [0 9]))
+    (is (heightmap-low-point? test-heightmap [2 2]))
+    (is (heightmap-low-point? test-heightmap [4 6]))
+    (is (not (heightmap-low-point? test-heightmap [0 0])))))
+
+(deftest heightmap-low-points-test
+  (testing "Finds all the low points in a heightmap"
+    (is (= [1 0 5 5] (heightmap-low-points test-heightmap)))))
