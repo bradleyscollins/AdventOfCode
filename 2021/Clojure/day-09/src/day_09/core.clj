d778e2c1e8e3d0139950a2a45b760a6f37a6a632
Brad Collins <bsc@housecollins.com>
2021-12-09 08:24:30 -0600

    2021, Clojure, Day 9: Setup

---
+++ 2021/Clojure/day-09/src/day_09/core.clj
@@ -0,0 +1,10 @@
+(ns day-09.core
+  (:gen-class)
+  (:require [clojure.java.io :as io]
+            [clojure.set     :as set]
+            [clojure.string  :as str]))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
