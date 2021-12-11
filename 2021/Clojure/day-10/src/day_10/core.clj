afd2c77d2333aef4752fa6b783f5b358c06b1a3d
Brad Collins <bsc@housecollins.com>
2021-12-11 11:31:59 -0600

    2021, Clojure, Day 10: Part 2 solved

--- 2021/Clojure/day-10/src/day_10/core.clj
+++ 2021/Clojure/day-10/src/day_10/core.clj
@@ -22,42 +22,76 @@
 (defn opener? [c]
   (contains? openers c))
 
+(defn build-completion [unmatched-opened]
+  (->> unmatched-opened
+       (map (comp str counterpart-of))
+       (str/join)))
+
 (defn check-syntax [line]
-  (loop [stack '()
-         code  line]
-    ;; (println "stack:       " stack)
-    ;; (println "code:        " code)
-    (let [last-char (first stack)
-          next-char (first code)]
-      ;; (println "Last char (top of stack):            " last-char)
-      ;; (println "Next char (in the remaining code):   " next-char)
-      (cond
-        (empty? code) [:ok]
-        (opener? next-char) (recur (cons next-char stack) (rest code))
-        :else (if (= last-char (counterpart-of next-char))
-                (recur (rest stack) (rest code))
-                [:corrupted {:expected (counterpart-of last-char),
-                             :found     next-char}])))
-    ))
+  (loop [[last-opened & rest-opened    :as opened] '()
+         [next-char   & remaining-code :as code]   line]
+    ;; (println "opened:" opened)
+    ;; (println "code:" code)
+    (cond
+      (and (empty? opened) (empty? code))
+      [:ok]
+
+      (empty? code)
+      [:incomplete {:expected (build-completion opened)}]
+
+      (opener? next-char)
+      (recur (cons next-char opened) remaining-code)
+
+      (= last-opened (counterpart-of next-char))
+      (recur rest-opened remaining-code)
+
+      :else
+      [:corrupted {:expected (counterpart-of last-opened) :found next-char}])))
 
 (def illegal-char-scores { \) 3
                            \] 57
                            \} 1197
-                           \> 25137 })
+                           \> 25137})
 
-(defn result->score [[status {illegal-char :found}]]
+(def missing-char-scores { \) 1
+                           \] 2
+                           \} 3
+                           \> 4 })
+
+(defn result->score [[status {illegal-char :found, expected :expected}]]
   ;; (println "Status:      " status)
   ;; (println "Illegal char:" illegal-char)
   (case status
-    :ok 0
-    :corrupted (illegal-char-scores illegal-char)))
+    :ok         0
+    :corrupted  (illegal-char-scores illegal-char)
+    :incomplete (reduce #(+ (* 5 %1) (missing-char-scores %2)) 0 expected)))
 
-(defn score-results [results]
-  (reduce + (map result->score results)))
+(def result-status first)
+
+(defn result-corrupted? [result]
+  (= :corrupted (result-status result)))
+
+(defn result-incomplete? [result]
+  (= :incomplete (result-status result)))
+
+(defn sum-corrupted-line-scores [results]
+  (->> results
+       (filter result-corrupted?)
+       (map result->score)
+       (reduce +)))
+
+(defn calc-median-incomplete-line-score [results]
+  (let [incomplete        (filter result-incomplete? results)
+        incomplete-scores (map result->score incomplete)
+        midpoint          (quot (count incomplete-scores) 2)]
+    (nth (sort incomplete-scores) midpoint)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (let [lines-of-code (read-input)
         results       (map check-syntax lines-of-code)]
-    (println "Total syntax error score:" (score-results results))))
+    (println "Total syntax error score for corrupted lines:"
+             (sum-corrupted-line-scores results))
+    (println "Median syntax score for incomplete lines:"
+             (calc-median-incomplete-line-score results))))
--- 2021/Clojure/day-10/test/day_10/core_test.clj
+++ 2021/Clojure/day-10/test/day_10/core_test.clj
@@ -25,9 +25,18 @@
     (is (= [:corrupted { :expected \>, :found \) }]
            (check-syntax "[<(<(<(<{}))><([]([]()")))
     (is (= [:corrupted { :expected \], :found \> }]
-           (check-syntax "<{([([[(<>()){}]>(<<{{")))))
+           (check-syntax "<{([([[(<>()){}]>(<<{{")))
+    (is (= [:incomplete {:expected "}}]])})]"}]
+           (check-syntax "[({(<(())[]>[[{[]{<()<>>")))))
 
 (deftest score-results-test
   (testing "Tallies up the total results of all the inputs"
-    (is (= 26397
-           (score-results (map check-syntax test-input))))))
+    (let [results (map check-syntax test-input)]
+      (testing "Sum of scores for corrupted lines"
+        (let [corrupted (filter result-corrupted? results)]
+          (is (= 26397
+                 (sum-corrupted-line-scores corrupted)))))
+
+      (testing "Median of (sorted) scores for incomplete lines"
+        (is (= 288957
+               (calc-median-incomplete-line-score results)))))))
