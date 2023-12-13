7c89649f564237ffd196b12de92e3024229c46db
Brad Collins <brad.collins@dynetics.com>
2023-12-13 17:19:18 -0600

    2015, day 3, part 2 in Ruby

--- 2015/Ruby/Day03/lib/program.rb
+++ 2015/Ruby/Day03/lib/program.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require_relative 'partner_visited_house_calculator'
 require_relative 'visited_house_calculator'
 
 class App
@@ -9,6 +10,12 @@ class App
     visit_calc = VisitedHouseCalculator.new([0, 0])
     houses_visited = visit_calc.calculate(moves)
     puts "How many houses receive at least one present? #{houses_visited}"
+
+    partner_visit_calc = PartnerVisitedHouseCalculator.new(
+      partners: 2,
+      initial_position: [0, 0])
+    houses_visited_by_santa_and_robo = partner_visit_calc.calculate(moves)
+    puts "How many houses receive at least one present from either Santa or Robo-Santa? #{houses_visited_by_santa_and_robo}"
   end
 end
 
